876fc418632537369dc4e651575550dae314bb0d
Serzas <j_sergej@yahoo.com>
2019-12-21 17:27:32 +0200

    executor bean

--- multiply-2D-matrices/src/main/java/com/serjer/Multiply2DMatricesApplication.java
+++ multiply-2D-matrices/src/main/java/com/serjer/Multiply2DMatricesApplication.java
@@ -1,7 +1,11 @@
 package com.serjer;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Multiply2DMatricesApplication {
@@ -9,5 +13,10 @@ public class Multiply2DMatricesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Multiply2DMatricesApplication.class, args);
 	}
+	
+	@Bean("fixedThreadPool")
+    public ExecutorService fixedThreadPool() {
+        return Executors.newFixedThreadPool(8);
+    }
 
 }
--- multiply-2D-matrices/src/main/java/com/serjer/service/MatrixService.java
+++ multiply-2D-matrices/src/main/java/com/serjer/service/MatrixService.java
@@ -2,17 +2,21 @@ package com.serjer.service;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.serjer.model.Matrix;
 
 @Service
 public class MatrixService {
-	
+
+	@Autowired
+	@Qualifier("fixedThreadPool")
+	private ExecutorService executor;
 
 	public int[][] generateMatrix(int size) {
 		int[][] matrix = new int[size][size];
@@ -40,8 +44,8 @@ public class MatrixService {
 
 	public int[][] multiplyMatricesAsync1(int[][] a, int[][] b, int size)
 			throws InterruptedException, ExecutionException {
+		
 		final int[][] result = new int[size][size];
-		final ExecutorService executor = Executors.newFixedThreadPool(8);
 
 		Future<Integer> future[][] = new Future[size][size];
 
@@ -59,14 +63,14 @@ public class MatrixService {
 		return result;
 	}
 
-	public int[][] multiplyMatricesAsync2(int[][] a, int[][] b, int size) throws InterruptedException {
-		final ExecutorService executorService = Executors.newCachedThreadPool();
-
+	public int[][] multiplyMatricesAsync2(int[][] a, int[][] b, int size) 
+			throws InterruptedException {
+	
 		final int[][] result = new int[size][size];
 
 		for (int i = 0; i < a.length; ++i) {
 			final int fi = i;
-			executorService.execute(() -> {
+			executor.execute(() -> {
 				for (int j = 0; j < b[0].length; ++j) {
 					for (int k = 0; k < a[0].length; ++k) {
 						result[fi][j] += a[fi][k] * b[k][j];
@@ -74,8 +78,8 @@ public class MatrixService {
 				}
 			});
 		}
-		executorService.shutdown();
-		executorService.awaitTermination(1, TimeUnit.DAYS);
+		executor.shutdown();
+		executor.awaitTermination(1, TimeUnit.DAYS);
 		return result;
 	}
 }
